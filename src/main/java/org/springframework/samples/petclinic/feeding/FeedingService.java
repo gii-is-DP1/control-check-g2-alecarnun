b7aa07b4dc02a38788e745ec002ad2141b7646a1
alecarnun <alecarnun@alum.us.es>
2022-01-14 10:24:15 +0100

    Final change

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,7 +2,11 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetType;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,9 +27,17 @@ public class FeedingService {
         return feedingRepo.findFeedingTypeByName(typeName);
     }
 
+    @Transactional(rollbackOn = UnfeasibleFeedingException.class)
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
-    }
+        Pet pet=p.getPet();
+        PetType petType = p.getFeedingType().getPetType();
+            if (!pet.getType().equals(petType)) {            	
+            	throw new UnfeasibleFeedingException();
+            
+
+        }else{
+        return feedingRepo.save(p);    
+    }}
 
     
 }
